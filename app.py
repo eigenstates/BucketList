eab41f05592002e163e490767ad004f528a75099
David V <eigenstates@yahoo.com>
2017-02-27 15:16:28 -0800

    Display drops

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, json, session, redirect
+from flask import Flask, render_template, request, json, session, redirect, jsonify
 from flaskext.mysql import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 #WSGI stuff.
 #flask_app = Flask('flaskapp')
 #app = flask_app.wsgi_app
 app = Flask(__name__)
-app.secrect_key = 'loggins messina'
+app.secret_key = 'loggins messina'
 
 #MySQL conf
 mySql = MySQL()
@@ -14,9 +14,6 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'solids'
 app.config['MYSQL_DATABASE_DB'] = 'taskapp'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mySql.init_app(app)
-
-conn = mySql.connect()
-cursor = conn.cursor()
 
 @app.route('/')
 def main():
@@ -32,8 +29,8 @@ def validateLogin():
         _userName = request.form['inputEmail']
         _password = request.form['inputPassword']
 
-        con = mySql.connect()
-        cursor = con.cursor()
+        conn = mySql.connect()
+        cursor = conn.cursor()
         cursor.callproc('sp_validateLogin',(_userName,))
         data = cursor.fetchall()
 
@@ -49,7 +46,7 @@ def validateLogin():
         return render_template('error.html', error = str(e))
     finally:
         cursor.close()
-        con.close()
+        conn.close()
 
     
 @app.route('/showSignup')
@@ -62,27 +59,85 @@ def signUp():
     _name = request.form['inputName']
     _email = request.form['inputEmail']
     _password = request.form['inputPassword']
-    _hashedPassword = generate_password_hash(_password)
 
     if _name and _email and _password:
-        cursor.callproc('sp_createUser', (_name, _email, _hashedPassword))
+        conn = mySql.connect()
+        cursor = conn.cursor()
+        hashedPassword = generate_password_hash(_password)
+        cursor.callproc('sp_createUser', (_name, _email, hashedPassword))
         data = cursor.fetchall()
         if len(data) is 0:
             conn.commit()
-            return json.dumps({'html': '<span> All Good </span>'})
+            return redirect('/userHome')
     else:
-        return json.dumps({'html': '<span> Please fill in all fields </span>'})
+        return render_template('error.html', error = "Please fill in all fields")
 
 @app.route('/userHome')
 def userHome():
     if session.get('user'):
         return render_template('userHome.html')
     else:
-        return render_template('error.html', gurgle = "Access DENIED!")
+        return render_template('error.html', error = "Access DENIED!")
 
 @app.route('/showAddBabble')
 def addDrop():
     return render_template('addBabble.html')
+
+@app.route('/addBabble', methods=['POST'])
+def addBabble():
+    try:
+        if session.get('user'):
+            _title = request.form['inputTitle']
+            _description = request.form['inputDescription']
+            _user = session.get('user')
+            conn = mySql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_addBabble',(_title, _description, _user))
+            data = cursor.fetchall()
+            if len(data) is 0:
+                conn.commit()
+                return redirect('/userHome')
+            else:
+                return render_template('error.html', error = "A vague error occured")
+        else:
+            return render_template('error.html', error = "Who do you think you are? I don't know.")
+    except Exception as e:
+        return render_template('error.html', error = str(e))
+    finally:
+        cursor.close()
+        conn.close()
+
+@app.route('/getBabbles')
+def getBabbles():
+    try:
+        if session.get('user'):
+            _user = session.get('user')
+
+            conn = mySql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_getBabblesByUser',(_user,))
+            babbles = cursor.fetchall()
+            print("ERE")
+            
+            babble_dict = []
+            for babble in babbles:
+                babble_item = {
+                    'Id': babble[0],
+                    'Title':babble[1],
+                    'Description':babble[2],
+                    'Date':babble[3]
+                }
+                babble_dict.append(babble_item)
+            print (babble_dict)
+            return jsonify({'babbles': babble_dict})
+        else:
+            return render_template('error.html', error = "Who do you think you are? I don't know.")
+    except Exception as e:
+        return render_template('error.html', error = str(e))
+    finally:
+        cursor.close()
+        conn.close()      
+
 
 @app.route('/logout')
 def logout():
